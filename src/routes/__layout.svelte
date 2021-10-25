05441332afa0d3385d1e88a2be84d65d3ba3b76a
Robert Boyd III <rob@robertcboyd.com>
2021-10-25 12:29:00 -0400

    Set up basic page

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+	<style>
+		@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap');
+		* {
+			margin: 0;
+			padding: 0;
+		}
+		body {
+			width: 100vw;
+			height: 100vh;
+			background-image: url('/shutterstock_624908471.jpg');
+			background-size: cover;
+			background-position: center;
+		}
+	</style>
+</svelte:head>
+
+<slot></slot>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,48 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+	<div><h1>Robert Boyd</h1><span class="pos">noun</span></div>
+	<p>
+		<span class="super">1</span> a curious student and software developer at the University of Central Florida passionate
+		about programming languages and the web
+	</p>
+</main>
+
+<style>
+	* {
+		font-family: Roboto;
+	}
+	main {
+		overflow: hidden;
+	}
+	h1 {
+		font-weight: 100;
+		font-size: 3em;
+		padding: 0.25em;
+		display: inline-block;
+	}
+	div {
+		white-space: nowrap;
+		margin: 0.25em;
+	}
+	.super {
+		vertical-align: super;
+		font-size: smaller;
+	}
+	.pos {
+		font-weight: bold;
+		font-size: 1.5em;
+		padding: 0.25em;
+	}
+	p {
+		padding: 0.5em;
+		margin: 0 1em;
+	}
+	@media screen and (min-width: 640px) {
+		h1 {
+			color: red;
+			font-size: 4em;
+		}
+		.pos {
+			font-size: 2em;
+		}
+	}
+</style>
