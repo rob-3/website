d573e83384447235279a1082b3bbfaa8367196c4
Robert Boyd III <rob@robertcboyd.com>
2021-12-01 12:31:14 -0500

    Fix 13

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -21,8 +21,7 @@
 			width: 100%;
 			height: 100%;
 			box-sizing: border-box;
-			background-attachment: fixed;
-			background-size: 100%;
+			background-size: 180%;
 		}
 		.all {
 			position: relative;
