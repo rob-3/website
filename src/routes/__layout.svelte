4626a512bf065506596cc48a680e2b87998784f9
Robert Boyd III <rob@robertcboyd.com>
2021-12-01 12:10:30 -0500

    Try fix 10

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -13,24 +13,18 @@
 			z-index: -1;
 			display: block;
 			top: 0px;
-			bottom: 0px;
 			left: 0px;
-			right: 0px;
 			background-image: url('/shutterstock_624908471.jpg');
 			background-size: cover;
 			background-repeat: no-repeat;
 			background-position: center;
-			width: 100vw;
-			height: 100vh;
-			overflow: auto;
-			scrollbar-gutter: stable;
+			width: 100%;
+			height: 100%;
 		}
 		.all {
 			position: absolute;
 			width: 100%;
 			height: 100%;
-			overflow: auto;
-			scrollbar-gutter: stable;
 		}
 		html {
 			overscroll-behavior: none;
