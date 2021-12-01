10f633e4253175567e51de2e8ce5e428a5eec90b
Robert Boyd III <rob@robertcboyd.com>
2021-12-01 11:37:47 -0500

    Try CSS tricks hack

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,14 +5,24 @@
 			margin: 0;
 			padding: 0;
 		}
-		body {
-			background-attachment: fixed;
+		.all::before {
+			content: '';
+			position: fixed;
+			z-index: -1;
+			display: block;
+			top: 0px;
+			bottom: 0px;
+			left: 0px;
+			right: 0px;
 			background-image: url('/shutterstock_624908471.jpg');
 			background-size: cover;
+			background-repeat: no-repeat;
 			background-position: center;
-			width: 100vw;
-			height: 100vh;
-			overflow: scroll;
+		}
+		.all {
+			position: absolute;
+			width: 100%;
+			height: 100%;
 		}
 		html {
 			overscroll-behavior: none;
@@ -20,4 +30,6 @@
 	</style>
 </svelte:head>
 
-<slot />
+<div class="all">
+	<slot />
+</div>
