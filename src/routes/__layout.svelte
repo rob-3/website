2d9583bf71710a05dd7f8f93cd1b4de4c8b0aad5
Robert Boyd III <rob@robertcboyd.com>
2021-12-01 12:02:40 -0500

    Add scrollbar-gutter

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -23,12 +23,14 @@
 			width: 100vw;
 			height: 100vh;
 			overflow: auto;
+			scrollbar-gutter: stable;
 		}
 		.all {
 			position: absolute;
 			width: 100%;
 			height: 100%;
 			overflow: auto;
+			scrollbar-gutter: stable;
 		}
 		html {
 			overscroll-behavior: none;
