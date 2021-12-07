d2ac074215ef7c6368af56c258d73bf4dcce9767
Robert Boyd III <rob@robertcboyd.com>
2021-12-07 11:53:33 -0500

    Add long repo descriptions

--- src/routes/_repos.ts
+++ src/routes/_repos.ts
@@ -1,7 +1,27 @@
 export default [
-	{ repo: 'rob-3/steel-lang', description: 'My programming language' },
-	{ repo: 'knighthacks/scythe', description: '' },
-	{ repo: 'knighthacks/lancelot' },
-	{ repo: 'knighthacks/hackathon-2021-frontend', description: 'This site was used as the main registration website for Knight Hacks 2021. It currently lives at <a href="https://2021.knighthacks.org/">https://2021.knighthacks.org/</a>' },
-	{ repo: 'knighthacks/club-site-2021' }
+	{
+		repo: 'rob-3/steel-lang',
+		description:
+			'Steel is a passion project programming language with some interesting design choices like true pass-by-reference semantics and rational, arbitrary precision numbers by default.'
+	},
+	{
+		repo: 'knighthacks/scythe',
+		description:
+			'Scythe is a library that wraps and extends the Discord.js API to provide a simplified interface to create <a href="https://discord.com/developers/docs/interactions/application-commands">Discord slash commands</a>.'
+	},
+	{
+		repo: 'knighthacks/lancelot',
+		description:
+			'Lancelot is the Knight Hacks Discord bot. It has commands for everything from server administration to flipping a coin.'
+	},
+	{
+		repo: 'knighthacks/hackathon-2021-frontend',
+		description:
+			'This site was used as the registration website for Knight Hacks 2021. It was built with React. An archived version currently lives at <a href="https://2021.knighthacks.org/">https://2021.knighthacks.org/</a>'
+	},
+	{
+		repo: 'knighthacks/club-site-2021',
+		description:
+			'This site is the Knight Hacks student organization club site. It is built with React and Gatsby, and lives at <a href="https://knighthacks.org">https://knighthacks.org</a>.'
+	}
 ];
