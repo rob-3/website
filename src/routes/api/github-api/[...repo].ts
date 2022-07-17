97a355bf5b3d3dcd6d2f66da81b9bbf049f11c60
Robert Boyd III <git@robertboyd.dev>
2022-07-17 11:21:53 -0700

    Bump npm deps

--- src/routes/api/github-api/[...repo].ts
+++ src/routes/api/github-api/[...repo].ts
@@ -1,4 +1,4 @@
-export async function get({ params: { repo } }: { params: { repo: string } }) {
+export async function GET({ params: { repo } }: { params: { repo: string } }) {
 	const data = await fetch(`https://api.github.com/repos/${repo}`, {
 		headers: {
 			authorization: `token ${import.meta.env.VITE_githubApiToken}`,
