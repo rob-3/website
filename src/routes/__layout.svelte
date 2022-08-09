b1c8a78705879b44044923f297d0913ccc7eb536
Robert Boyd III <git@robertboyd.dev>
2022-08-09 00:52:46 -0400

    Fix footer on very narrow screens

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,9 +5,9 @@
 
 <div>
 	<slot />
-	<footer class="text-black flex justify-center p-6">
-		<a class="hover:opacity-60 mx-8" href={links.github}><i class="fab fa-github fa-5x" /></a>
-		<a class="hover:opacity-60 mx-8" href="/"><i class="fas fa-home fa-5x"></i></a>
-		<a class="hover:opacity-60 mx-8" href={links.linkedin}><i class="fab fa-linkedin fa-5x" /></a>
+	<footer class="text-black flex justify-around sm:justify-center sm:gap-10 py-6">
+		<a class="hover:opacity-60" href={links.github}><i class="fab fa-github fa-5x" /></a>
+		<a class="hover:opacity-60" href="/"><i class="fas fa-home fa-5x"></i></a>
+		<a class="hover:opacity-60" href={links.linkedin}><i class="fab fa-linkedin fa-5x" /></a>
 	</footer>
 </div>
