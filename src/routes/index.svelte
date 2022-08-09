6977da40d0ca1d28101b4249fcc4ba59ac69a2e1
Robert Boyd III <git@robertboyd.dev>
2022-08-08 23:46:28 -0400

    Format code

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,15 +3,18 @@
 	import YouTubeVideo from '../components/YouTubeVideo.svelte';
 	import talks from './_talks';
 	import repos from './_repos';
-	import links from './_links'
+	import links from './_links';
 </script>
 
 <svelte:head>
 	<script src="https://kit.fontawesome.com/b6461ef53d.js" crossorigin="anonymous"></script>
 </svelte:head>
 <header class="flex items-center w-fit m-auto my-4">
-	<img src="/robert-boyd.jpeg" alt="Robert Boyd"
-		class="inline-block rounded-full w-24 h-24 sm:w-28 sm:h-28">
+	<img
+		src="/robert-boyd.jpeg"
+		alt="Robert Boyd"
+		class="inline-block rounded-full w-24 h-24 sm:w-28 sm:h-28"
+	/>
 	<h1>Robert Boyd</h1>
 </header>
 <main class="max-w-max m-auto">
@@ -29,14 +32,15 @@
 			<div class="flex justify-center w-full">
 				<YouTubeVideo src={talks[0]} />
 			</div>
-			<a class="
+			<a
+				class="
 				block text-white border border-solid border-black px-4
 				py-3 talk-link m-4 rounded-lg no-underline bg-black
 				hover:bg-[rgba(0,0,0,0)] hover:text-black transition-all
-				" 
+				"
 				sveltekit:prefetch
-				href="/talks"
-			>All Talks<i class="ml-2 fas fa-angle-double-right"></i></a>
+				href="/talks">All Talks<i class="ml-2 fas fa-angle-double-right" /></a
+			>
 		</section>
 	</div>
 </main>
