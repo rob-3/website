c7662bf58873da10b04282935a925856ed71d820
Robert Boyd III <git@robertboyd.dev>
2022-08-22 20:59:35 -0400

    Add labels to footer links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,28 @@
 <script lang="ts">
-	import "../app.css";
+	import '../app.css';
 	import links from './_links';
 </script>
 
 <div>
 	<slot />
 	<footer class="text-black flex justify-around sm:justify-center sm:gap-10 py-6">
-		<a class="hover:opacity-60" href={links.github}><i class="fab fa-github fa-5x" /></a>
-		<a class="hover:opacity-60" href="/"><i class="fas fa-home fa-5x"></i></a>
-		<a class="hover:opacity-60" href={links.linkedin}><i class="fab fa-linkedin fa-5x" /></a>
+		<a class="hover:opacity-60" href={links.github}>
+			<div class="flex flex-col items-center">
+				<i class="fab fa-github fa-5x" />
+				<span>GitHub</span>
+			</div>
+		</a>
+		<a class="hover:opacity-60" href="/">
+			<div class="flex flex-col items-center">
+				<i class="fas fa-home fa-5x" />
+				<span>Home</span>
+			</div>
+		</a>
+		<a class="hover:opacity-60" href={links.linkedin}>
+			<div class="flex flex-col items-center">
+				<i class="fab fa-linkedin fa-5x" />
+				<span>LinkedIn</span>
+			</div>
+		</a>
 	</footer>
 </div>
