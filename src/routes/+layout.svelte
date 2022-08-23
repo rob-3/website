63091675080123f1f4a7b3691bcc87c15e2b23cd
Robert Boyd III <git@robertboyd.dev>
2022-08-22 21:13:09 -0400

    Remove underline from icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,19 +8,19 @@
 	<footer class="text-black flex justify-around sm:justify-center sm:gap-10 py-6">
 		<a class="hover:opacity-60" href={links.github}>
 			<div class="flex flex-col items-center">
-				<i class="fab fa-github fa-5x" />
+				<i class="fab fa-github fa-5x no-underline" />
 				<span>GitHub</span>
 			</div>
 		</a>
 		<a class="hover:opacity-60" href="/">
 			<div class="flex flex-col items-center">
-				<i class="fas fa-home fa-5x" />
+				<i class="fas fa-home fa-5x no-underline" />
 				<span>Home</span>
 			</div>
 		</a>
 		<a class="hover:opacity-60" href={links.linkedin}>
 			<div class="flex flex-col items-center">
-				<i class="fab fa-linkedin fa-5x" />
+				<i class="fab fa-linkedin fa-5x no-underline" />
 				<span>LinkedIn</span>
 			</div>
 		</a>
