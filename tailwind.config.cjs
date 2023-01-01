8a90a4947ff33ff25861eaa1b1463ff239f842e7
Robert Boyd III <git@robertboyd.dev>
2022-12-31 21:51:59 -0500

    Revert "Switch to grey color theme"
    This reverts commit ea64d5ff1492a858ef89ed0620a3fd357378afcb.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,12 +3,7 @@ module.exports = {
   important: true,
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {
-      colors: {
-        bluegrey: "#3D4351",
-        offwhite: "#F2F4F8"
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
